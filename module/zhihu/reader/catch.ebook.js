49287dd3c98c9b79f146ebf381e576b5e432d149
469577278@qq.com <469577278@qq.com>
2023-05-18 00:37:37 +0800

    update

--- module/zhihu/reader/catch.ebook.js
+++ module/zhihu/reader/catch.ebook.js
@@ -4,3 +4,17 @@ chrome.runtime.sendMessage({greeting: '你好，我是content-script呀，我主
     console.log(response);
     console.log("---------- catch.ebook.js end ----------");
 });
+
+
+setTimeout(()=>{
+    var titleArr = document.getElementsByClassName("secondTitle-1")
+    if(!titleArr) titleArr = document.getElementsByClassName("firstTitle-1")
+    var title = titleArr.outerText;
+    var content = document.getElementsByClassName("content")[0].outerHTML;
+    chrome.runtime.sendMessage({title: title, content: content}, ()=>{})
+
+    var nextA = document.getElementById("button-next-chapter").getElementsByTagName("a")[0];
+    if(nextA.className.indexOf("disabled")==-1) {
+        nextA.click();
+    }
+}, 5000);
